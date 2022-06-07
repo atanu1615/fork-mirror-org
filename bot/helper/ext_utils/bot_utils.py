1e6cf9518bd2585e82872e38ce55de22592c41ca
atanu1615 <84491066+atanu1615@users.noreply.github.com>
2022-06-07 13:16:06 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -118,13 +118,8 @@ def get_progress_bar_string(status):
     return p_str
     
     
-    cPart = p % 8 - 1
-    p_str = '⬤' * cFull
-    if cPart >= 0
-        p_str += PROGRESS_INCOMPLETE[cPart]
-    p_str += '○' * (PROGRESS_MAX_SIZE - cFull)
-    p_str = f"「{p_str}」"
-    return p_str
+
+
 
 # def get_progress_bar_string(status):
 #    completed = status.processed_bytes() / 8
