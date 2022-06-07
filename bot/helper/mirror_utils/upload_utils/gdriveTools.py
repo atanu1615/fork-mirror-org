d4c3a34d69d947f3a1503f6502172d8b5ba29f44
atanu1615 <84491066+atanu1615@users.noreply.github.com>
2022-06-07 18:30:53 +0530

    Update gdriveTools.py

--- bot/helper/mirror_utils/upload_utils/gdriveTools.py
+++ bot/helper/mirror_utils/upload_utils/gdriveTools.py
@@ -164,7 +164,7 @@ class GoogleDriveHelper:
         # File body description
         file_metadata = {
             'name': file_name,
-            'description': 'Uploaded by Mirror-leech-telegram-bot',
+            'description': 'Uploaded by Kal Ho Na Ho',
             'mimeType': mime_type,
         }
         if parent_id is not None:
