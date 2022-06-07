e7341b2af4482db6ae86f6086a661277368fe736
atanu1615 <84491066+atanu1615@users.noreply.github.com>
2022-06-07 13:11:33 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -103,10 +103,19 @@ def getAllDownload(req_status: str):
                     return dl
     return None
 
+def get_progress_bar_string(status):
+    completed = status.processed_bytes() / 8
+    total = status.size_raw() / 8
     p = 0 if total == 0 else round(completed * 100 / total)
     p = min(max(p, 0), 100)
     cFull = p // 8
-
+    cPart = p % 8 - 1
+    p_str = '⬤' * cFull
+    if cPart >= 0:
+        p_str += PROGRESS_INCOMPLETE[cPart]
+    p_str += '○' * (PROGRESS_MAX_SIZE - cFull)
+    p_str = f"「{p_str}」"
+    return p_str
     
     
     cPart = p % 8 - 1
@@ -117,16 +126,16 @@ def getAllDownload(req_status: str):
     p_str = f"「{p_str}」"
     return p_str
 
-def get_progress_bar_string(status):
-    completed = status.processed_bytes() / 8
-    total = status.size_raw() / 8
-    p = 0 if total == 0 else round(completed * 100 / total)
-    p = min(max(p, 0), 100)
-    cFull = p // 8
-    p_str = '■' * cFull
-    p_str += '□' * (12 - cFull)
-    p_str = f"[{p_str}]"
-    return p_str
+# def get_progress_bar_string(status):
+#    completed = status.processed_bytes() / 8
+#    total = status.size_raw() / 8
+#    p = 0 if total == 0 else round(completed * 100 / total)
+#    p = min(max(p, 0), 100)
+#    cFull = p // 8
+#    p_str = '■' * cFull
+#    p_str += '□' * (12 - cFull)
+#    p_str = f"[{p_str}]"
+#    return p_str
 
 def get_readable_message():
     with download_dict_lock:
